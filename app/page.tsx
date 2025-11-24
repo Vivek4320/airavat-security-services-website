30011af9e4a09bfd8152331c759d72721d1c992c
Vivek4320 <vivekpankhaniya43@gmail.com>
2025-11-24 14:04:47 +0530

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -1256,7 +1256,7 @@ export default function Home() {
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                   </svg>
-                  <span className="group-hover:underline">airavat1@gmail.com</span>
+                  <span className="group-hover:underline">airavats1@gmail.com</span>
                 </a>
 
                 {/* Social Media Icons */}
